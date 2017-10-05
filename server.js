bd32a5486adf3aaf711da53d1313c4a12a672413
Igor Orszanski <igor.orszanski@gmail.com>
2017-10-05 16:34:00 +0200

    add server

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use((req, res, next) => {
+  if (req.headers['x-forwarded-proto'] === 'https') {
+    res.redirect('http://' + req.hostname + req.url);
+  } else {
+    next();
+  }
+});
+app.use(express.static('dist'));
+
+app.listen(PORT, () => {
+  console.log('server up');
+});
